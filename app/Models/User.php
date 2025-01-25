2bfea9f70cbacea1994c032a37d8947bfb5a7276
seiis <seiisland@gmail.com>
2025-01-25 10:09:43 +0900

    postコントローラ作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,5 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);//作成
+    }
 }
