7ac8b1639a3e4ce009a418ae8fcfd1ec215db760
seiis <seiisland@gmail.com>
2025-01-25 18:24:54 +0900

    更新機能実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,8 +39,9 @@ class PostController extends Controller
         $post->user_id = $request->user()->id;
 
         $file = $request->file('image'); //create.blade.phpのname属性
-        $post->image = date('YmdHis') . '_' . $file->getClientOriginalName();
+        // $post->image = date('YmdHis') . '_' . $file->getClientOriginalName();
         //Ymdは分秒なし
+        $post->image = self::createFileName($file);
 
         // トランザクション開始
         +DB::beginTransaction();
@@ -62,16 +63,18 @@ class PostController extends Controller
         }
 
         return redirect()
-            ->route('posts.show', $post);
+            ->route('posts.show', $post)
+            ->with("notice", "記事を登録しました"); //withメソッドを使ってお知らせ機能が実装できる｡
         //reture redirect(route("posts.show",$post));メモアプリはこの記述だがどちらでもよい
     }
-
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
         //
+        $post = Post::find($id);
+        return view("posts.show", compact("post"));
     }
 
     /**
@@ -80,6 +83,8 @@ class PostController extends Controller
     public function edit(string $id)
     {
         //
+        $post = Post::find($id);
+        return view('posts.edit', compact('post'));
     }
 
     /**
@@ -87,8 +92,53 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, string $id)
     {
-        //
+        $post = Post::find($id);
 
+        if ($request->user()->cannot('update', $post)) {
+            return redirect()->route('posts.show', $post)
+                ->withErrors('自分の記事以外は更新できません');
+        }
+
+        $file = $request->file('image');
+        if ($file) {
+            $delete_file_path = 'images/posts/' . $post->image;
+            // $post->image = date('YmdHis') . '_' . $file->getClientOriginalName();
+            $post->image = self::createFileName($file);
+        }
+        $post->fill($request->all());
+
+        // トランザクション開始
+        DB::beginTransaction();
+        try {
+            // 更新
+            $post->save();
+
+            if ($file) {
+                // 画像アップロード
+                if (!Storage::putFileAs('images/posts', $file, $post->image)) {
+                    // 例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの保存に失敗しました。');
+                }
+
+                // 画像削除
+                if (!Storage::delete($delete_file_path)) {
+                    //アップロードした画像を削除する
+                    Storage::delete('images/posts/' . $post->image);
+                    //例外を投げてロールバックさせる
+                    throw new \Exception('画像ファイルの削除に失敗しました。');
+                }
+            }
+
+            // トランザクション終了(成功)
+            DB::commit();
+        } catch (\Exception $e) {
+            // トランザクション終了(失敗)
+            DB::rollback();
+            return back()->withInput()->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('posts.show', $post)
+            ->with('notice', '記事を更新しました');
     }
 
     /**
@@ -97,5 +147,10 @@ class PostController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    private static function createFileName($file)
+    {
+        return date('YmdHis') . '_' . $file->getClientOriginalName();
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
@@ -14,5 +15,10 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class); //作成
+    }
+    //public function image_url() (1)こちらでも良い
+    public function getImageUrlAttribute()
+    {
+        return Storage::url('images/posts/' . $this->image); //作成
     }
 }
